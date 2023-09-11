6d2cc4e1a358254d382e7d30bb2f60d3b18dfeec
Felipe Brevio <felipebrevio05@gmail.com>
2023-09-11 17:04:28 -0300

    react app

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StatusBar } from "react-native";
+import { WebView } from 'react-native-webview';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar backgroundColor='#23a2f6'/>
+        <WebView 
+          source={{ uri: 'https://trispectacle-93b2a62d86f1.herokuapp.com/' }} 
+        />
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
